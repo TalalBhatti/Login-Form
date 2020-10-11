a5358bd5b6d30acaf341078b19a5564d3564186c
TalalBhatti <talalabhatti@gmail.com>
2020-10-11 23:25:05 +0500

    Authentication added

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Login-Form
-This is a simple form with 2 input fields and can also be altered to your conditions
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
-app.get('/',(req,res)=>{
-    res.send("Hello this is my first Backend server")
+const User = {
+    id: 1,
+    email: "Furqanali@admin.com",
+    password: "Ajkstio94u3D"
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/login',(req, res) => {
+    if((req.body.email == User.email) && (req.body.password == User.password)){
+        res.redirect('/dashboard');
+    }
+    else{
+        console.log("incorrect");
+    }
 })
+app.get('/dashboard',(req,res)=>{
+    res.sendFile('dashboard.html')
+})
+
 
 const port = process.env.PORT || 5000;
 
